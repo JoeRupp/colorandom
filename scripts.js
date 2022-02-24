9774268a641be5b9acc49c6fad6bf0bb232e9d62
Joe Rupp <joerpp8@gmail.com>
2022-02-24 14:59:11 -0700

    Add save palette functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
+var currentPalette;
+var color1;
+var color2;
+var color3;
+var color4;
+var color5;
+
+var savedPalettes = [];
 var hexCharacters = ['A','B','C','D', 'E', 'F', '0', '1', '2','3', '4', '5', '6', '7', '8', '9'];
+
 var newPaletteButton = document.querySelector('.new-palette');
 var savePaletteButton = document.querySelector('.save-palette');
 var lockOneButton = document.querySelector('.lock-1');
@@ -6,34 +15,32 @@ var lockTwoButton = document.querySelector('.lock-2');
 var lockThreeButton = document.querySelector('.lock-3');
 var lockFourButton = document.querySelector('.lock-4');
 var lockFiveButton = document.querySelector('.lock-5');
+
 var colorOneDiv = document.querySelector('.color-one');
 var colorTwoDiv = document.querySelector('.color-two');
 var colorThreeDiv = document.querySelector('.color-three');
 var colorFourDiv = document.querySelector('.color-four');
 var colorFiveDiv = document.querySelector('.color-five');
+
 var hexCodeOne = document.querySelector('.hex-code-1');
 var hexCodeTwo = document.querySelector('.hex-code-2');
 var hexCodeThree = document.querySelector('.hex-code-3');
 var hexCodeFour = document.querySelector('.hex-code-4');
 var hexCodeFive = document.querySelector('.hex-code-5');
 
+var savedPaletteSection = document.querySelector('.saved-section');
+
 window.addEventListener('load', createPalette);
+window.addEventListener('load', generateColors);
+
 newPaletteButton.addEventListener('click', generateColors);
 savePaletteButton.addEventListener('click', savePalette);
+
 lockOneButton.addEventListener('click', lockColor);
 lockTwoButton.addEventListener('click', lockColor);
 lockThreeButton.addEventListener('click', lockColor);
 lockFourButton.addEventListener('click', lockColor);
 lockFiveButton.addEventListener('click', lockColor);
-
-window.addEventListener('load', generateColors);
-
-var currentPalette
-var color1
-var color2
-var color3
-var color4
-var color5
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
@@ -49,11 +56,36 @@ function generateHexCode() {
 };
 
 function createPalette() {
-  currentPalette = new Palette(color1, color2, color3, color4, color5);
+  currentPalette = new Palette();
 }
 
-function savePalette() {
+// color1, color2, color3, color4, color5
 
+function savePalette() {
+  savedPalettes.unshift(currentPalette);
+  displaySavedPalettes();
+  currentPalette = new Palette();
+};
+
+function displaySavedPalettes() {
+  var userSavePalettes = [];
+  for (var i = 0; i < savedPalettes.length; i++) {
+    userSavePalettes += `<section class="saved-palette" id="${savedPalettes[i].id}">
+    <div class="saved-color"></div>
+    <div class="saved-color"></div>
+    <div class="saved-color"></div>
+    <div class="saved-color"></div>
+    <div class="saved-color"></div>
+    <img class="trash-icon" src="https://www.iconpacks.net/icons/1/free-trash-icon-347-thumb.png" alt="delete button">
+    </section>`
+  }
+  savedPaletteSection.innerHTML = userSavePalettes;
+}
+
+function deletePalette(event) {
+   if (event.target.className === '.trash') {
+
+   }
 }
 
 function generateColors() {
