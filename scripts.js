56264e9bdf654fd503fb0a16973174c6deb6f76d
Lourdes Benites <lourdesben95@gmail.com>
2022-02-24 12:02:23 -0700

    Add random color display

--- scripts.js
+++ scripts.js
@@ -6,6 +6,16 @@ var lockTwoButton = document.querySelector('.lock-2');
 var lockThreeButton = document.querySelector('.lock-3');
 var lockFourButton = document.querySelector('.lock-4');
 var lockFiveButton = document.querySelector('.lock-5');
+var colorOneDiv = document.querySelector('.color-one');
+var colorTwoDiv = document.querySelector('.color-two');
+var colorThreeDiv = document.querySelector('.color-three');
+var colorFourDiv = document.querySelector('.color-four');
+var colorFiveDiv = document.querySelector('.color-five');
+var hexCodeOne = document.querySelector('.hex-code-1');
+var hexCodeTwo = document.querySelector('.hex-code-2');
+var hexCodeThree = document.querySelector('.hex-code-3');
+var hexCodeFour = document.querySelector('.hex-code-4');
+var hexCodeFive = document.querySelector('.hex-code-5');
 
 window.addEventListener('load', createPalette);
 newPaletteButton.addEventListener('click', generateColors);
@@ -16,9 +26,14 @@ lockThreeButton.addEventListener('click', lockColor);
 lockFourButton.addEventListener('click', lockColor);
 lockFiveButton.addEventListener('click', lockColor);
 
-
+window.addEventListener('load', generateColors);
 
 var currentPalette
+var color1
+var color2
+var color3
+var color4
+var color5
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
@@ -34,7 +49,7 @@ function generateHexCode() {
 };
 
 function createPalette() {
-  currentPalette = new Palette()
+  currentPalette = new Palette(color1, color2, color3, color4, color5);
 }
 
 function savePalette() {
@@ -42,15 +57,25 @@ function savePalette() {
 }
 
 function generateColors() {
-  currentPalette.color1 = new Color(generateHexCode())
-  currentPalette.color2 = new Color(generateHexCode())
-  currentPalette.color3 = new Color(generateHexCode())
-  currentPalette.color4 = new Color(generateHexCode())
-  currentPalette.color5 = new Color(generateHexCode())
+  color1 = new Color(generateHexCode())
+  colorOneDiv.style.backgroundColor = color1.hexCode
+  hexCodeOne.innerText = color1.hexCode
+  color2 = new Color(generateHexCode())
+  colorTwoDiv.style.backgroundColor = color2.hexCode
+  hexCodeTwo.innerText = color2.hexCode
+  color3 = new Color(generateHexCode())
+  colorThreeDiv.style.backgroundColor = color3.hexCode
+  hexCodeThree.innerText = color3.hexCode
+  color4 = new Color(generateHexCode())
+  colorFourDiv.style.backgroundColor = color4.hexCode
+  hexCodeFour.innerText = color4.hexCode
+  color5 = new Color(generateHexCode())
+  colorFiveDiv.style.backgroundColor = color5.hexCode
+  hexCodeFive.innerText = color5.hexCode
 }
 
 function lockColor() {
-  
+
 }
 
 // function replaceColor() {
